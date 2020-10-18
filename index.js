6eb90c52ec9f1c346262e1429b6ccb484008d6c7
Lisa <lisafilliette@gmail.com>
2020-10-18 23:07:39 +0200

    appel api index.html et lien vers page 2

---
+++ index.js
@@ -0,0 +1,40 @@
+function createNode(element) {
+	return document.createElement(element);
+}
+
+function append(parent, el) {
+	return parent.appendChild(el);
+}
+// APPEL A L'API
+fetch("http://localhost:3000/api/teddies")
+	.then((resp) => resp.json())
+	.then(function (data) {
+		let container = document.getElementById("oursons");
+		data.forEach(function (ourson) {
+			let article = createNode("article"),
+				img = createNode("img"),
+				h1 = createNode("h1"),
+				p = createNode("p"),
+				span = createNode("span");
+			img.src = ourson.imageUrl;
+			h1.innerHTML = `${ourson.name}`;
+			p.innerHTML = `${ourson.description} `;
+			span.innerHTML = `${ourson.price} `;
+
+			const btn = document.createElement("button"); // création du bouton
+			btn.textContent = "plus d'informations...";
+			btn.addEventListener("click", function () {
+				location = "produit.html?id=5be9c8541c9d440000665243";
+			});
+
+			append(article, img);
+			append(article, h1);
+			append(article, p);
+			append(article, span);
+			append(article, btn);
+			append(container, article);
+		});
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
